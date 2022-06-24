b1e10315a6da8a0caad00cbc7a50222f6461a14f
AnnaRabychina <nutochka@tut.by>
2022-06-25 01:22:07 +0300

    feat: add drawig square and rectangle

--- src/Utils/drawCommands.ts
+++ src/Utils/drawCommands.ts
@@ -6,6 +6,12 @@ export const drawCommands = (command: string, params: string[]) => {
         case 'draw_circle':
             drawCircle(params[0]);
             break;
+        case 'draw_rectangle':
+            drawRectangle(params[0], params[1]);
+            break;
+        case 'draw_square':
+            drawRectangle(params[0], params[0]);
+            break;
     }
 }
 
@@ -15,5 +21,15 @@ const drawCircle = (radius: string): void => {
         robot.dragMouse(x + +radius * Math.cos(i) - +radius, y + +radius * Math.sin(i));
         robot.mouseToggle('down', 'left');
     }
+    robot.mouseToggle('up');
+}
+
+const drawRectangle = (width: string, length: string): void => {
+    const { x, y } = robot.getMousePos();
+    robot.mouseToggle('down', 'left');
+    robot.moveMouseSmooth(x + +width, y);
+    robot.moveMouseSmooth(x + +width, y + +length);
+    robot.moveMouseSmooth(x, y + +length);
+    robot.moveMouseSmooth(x, y);
     robot.mouseToggle('up');
 }
